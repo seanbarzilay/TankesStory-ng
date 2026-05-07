4578abd2db0bd69da1f03bba98643fc4cab9f3c9
sbarzilay <sesnbarzilay@gmail.com>
2026-05-07 23:26:43 +0300

    Delete IRC bridge code (replaced by Telegram bridge)

--- src/main/java/net/server/chat/irc/IrcSender.java
+++
@@ -1,9 +0,0 @@
-package net.server.chat.irc;
-
-public interface IrcSender {
-    /** Returns true if the line was queued; false if dropped (queue full / not connected). */
-    boolean enqueue(String rawIrcLine);
-
-    /** Current bot nick, used for echo-loop suppression. */
-    String currentNick();
-}
--- src/main/java/net/server/chat/irc/WorldBroadcaster.java
+++
@@ -1,8 +0,0 @@
-package net.server.chat.irc;
-
-import net.packet.Packet;
-
-@FunctionalInterface
-public interface WorldBroadcaster {
-    void broadcast(int worldId, Packet packet);
-}
